296e6ca0e1bfdfb411fc903e048fc7dabca1b171
Jasur Ungarov <example@gmail.com>
2024-11-12 00:23:19 +0600

    authMiddlware

---
+++ app/[lng]/(root)/_components/navbar.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import Logo from '@/components/shared/logo'
+import ModeToggle from '@/components/shared/mode-toggle'
+import { Button } from '@/components/ui/button'
+import { navLinks } from '@/constants'
+import { ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import GlobalSearch from './global-search'
+import LanguageDropdown from '@/components/shared/language-dropdown'
+import { SignInButton, SignUpButton, SignedIn, SignedOut } from '@clerk/nextjs'
+import UserBox from '@/components/shared/user-box'
+import useTranslate from '@/hooks/use-translate'
+
+function Navbar() {
+	const t = useTranslate()
+
+	return (
+		<div className='fixed inset-0 z-40 h-20 bg-background/70 backdrop-blur-xl'>
+			<div className='container mx-auto flex h-full max-w-7xl items-center justify-between border-b'>
+				<div className='flex items-center gap-4'>
+					<Logo />
+					<div className='flex items-center gap-3 border-l pl-2'>
+						{navLinks.map(nav => (
+							<Link
+								href={`/${nav.route}`}
+								key={nav.route}
+								className='font-medium transition-all hover:text-blue-500 hover:underline'
+							>
+								{t(nav.name)}
+							</Link>
+						))}
+					</div>
+				</div>
+
+				<div className='flex items-center gap-2'>
+					<div className='flex items-center gap-2 border-r pr-3'>
+						<GlobalSearch />
+						<LanguageDropdown />
+						<Button size={'icon'} variant={'ghost'}>
+							<ShoppingCart />
+						</Button>
+						<ModeToggle />
+					</div>
+					<SignedIn>
+						<UserBox />
+					</SignedIn>
+					<SignedOut>
+						<SignInButton mode='modal'>
+							<Button variant={'ghost'} size={'lg'} rounded={'full'}>
+								Log in
+							</Button>
+						</SignInButton>
+						<SignUpButton mode='modal'>
+							<Button size={'lg'} rounded={'full'}>
+								Sign Up
+							</Button>
+						</SignUpButton>
+					</SignedOut>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+import { Contact, Home, ListVideo, Rss } from 'lucide-react'
+
+export const navLinks = [
+	{ route: '', name: 'navLink1', icon: Home },
+	{ route: 'courses', name: 'navLink2', icon: ListVideo },
+	{ route: 'blogs', name: 'navLink3', icon: Rss },
+	{ route: 'contact', name: 'navLink4', icon: Contact },
+]
+
+export const lngs = [
+	{ route: 'en', label: 'English' },
+	{ route: 'uz', label: "O'zbekcha" },
+	{ route: 'ru', label: 'Русский' },
+	{ route: 'tr', label: 'Türkçe' },
+]
